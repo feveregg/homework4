4ae1e8cc3e05605ce3b2f378839a25ac9d9a6419
feveregg <wk45@afsconnect2.njit.edu>
2017-10-02 16:51:59 -0400

    Replace “-“ in  with “/“

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 
 <?php
+/*
 $date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
 
@@ -9,5 +10,85 @@ echo "The value of \$tar: ".$tar."<br>";
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
+*/
 
+
+$obj = new main();
+
+class main {
+
+   private $html;
+
+//Start
+   public function __construct() {
+   
+   $date =  date('Y-m-d', time());
+   
+   $tar = "2017/05/24";
+  
+   $year = array("2012", "396", "300","2000", "1100", "1089");
+
+
+//1   
+   $this->html .= htmlTags::headingOne("Replace - in \$date with /");
+   $this->html .= stringFunctions::replace("-", "/", $date);
+
+   }
+//End
+   
+   public function __destruct() {
+   
+   $this->html .= stringFunctions::printThis($this->html);
+   }
+
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+  class htmlTags {
+
+    static public function horizontalRule() {
+
+    return "<hr>";
+
+    }
+
+
+    static public function headingOne($input) {
+
+    return "<h1>  $input  </h1>";
+
+    }
+  
+
+  }
+
+
+
+  class stringFunctions {
+
+    static public function printThis ($input) {
+    
+    return print($input);
+    
+    }
+
+
+    static public function replace ($input1, $input2, $input3) {
+    
+    return str_replace($input1, $input2, $input3);
+    
+    }
+  
+  
+  }
 ?>
