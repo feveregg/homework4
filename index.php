eb9d57a76befd512fc6305753bb2c951f00eb56a
feveregg <wk45@afsconnect2.njit.edu>
2017-10-02 22:59:12 -0400

    finish 5,6

--- index.php
+++ index.php
@@ -12,21 +12,39 @@ class main {
    $date =  date('Y-m-d', time());
    $tar = "2017/05/24"; 
    $year = array("2012", "396", "300","2000", "1100", "1089");
+   
 
-//1   
+//2   
    $this->html .= htmlTags::headingOne("2. Replace - in \$date with /");
    $this->html .= stringFunctions::replace("-", "/", $date);
    $this->html .= htmlTags::horizontalRule();
 
-//2
+//3
    $this->html .= htmlTags::headingOne("3. Compare \$date with \$tar");
    $this->html .= ifFunction::ifElse(strtotime($tar)-strtotime($date)>0, "the future", 
     strtotime($tar)-strtotime($date)<0, "the past", strtotime($tar)-strtotime($date)==0, "Oops!");
    $this->html .= htmlTags::horizontalRule();
 
+//4
+   $newDate = stringFunctions::replace("-", "/", $date);
+   $this->html .= htmlTags::headingOne("4. Search for / in \$date");
+   $this->html .= htmlTags::horizontalRule();
+
+//5
+   $this->html .= htmlTags::headingOne("5. Count the number of words");
+   $this->html .= stringFunctions::countWord($date);
+   $this->html .= htmlTags::horizontalRule();
+
+//6
+   $this->html .= htmlTags::headingOne("6. Return the length of a string");
+   $this->html .= stringFunctions::stringLength($date);
+   $this->html .= htmlTags::horizontalRule();
+
+
+
    }
 
-
+   
 
 //End   
    public function __destruct() {
@@ -74,7 +92,19 @@ class main {
     
     }
   
-  
+    
+    static public function countWord ($input) {
+    
+    return str_word_count($input);
+    
+    }
+
+    
+    static public function stringLength ($input) {
+    
+    return strlen($input);
+    
+    }
   }
 
 
@@ -96,7 +126,22 @@ class main {
      
      }
 
-/*
+
+/*  
+  class loopFunction {
+  
+     static public function whileFunction ($input1, $input2) {
+     
+     while($input1) {
+     
+     return $input2; 
+    
+    } 
+     
+     }
+  
+  }
+
   class otherFunctions {
 
 
@@ -108,5 +153,10 @@ class main {
 */ 
  
  } 
- 
+
+
+
+
+
+
 ?>
